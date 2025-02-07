328e324ba994adff34da27992ddc1e0e44c78962
Inma Palma <inma.palma99@gmail.com>
2025-02-07 13:08:52 +0100

    Actualizar para obtener no permitir pulsar en el boton hasta que rellene algo en el usuario

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 const passwordInput = document.getElementById("password");
 const feedback = document.getElementById("feedback");
 const boton = document.getElementById("boton");
+const usuario = document.getElementById("usuario");
+
+usuario.addEventListener("input", validar);
 
 passwordInput.addEventListener("input", () => {
     const password = passwordInput.value;
     if (password.length <= 7) {
         feedback.textContent = "La contraseña no es segura";
         feedback.className = "mal";
-        boton.setAttribute("disabled", "true");
     } else if (password.length >= 11) {
         feedback.textContent = "La contraseña es segura";
         feedback.className = "bien";
-        //habilitar boton
-        boton.removeAttribute("disabled");
     } else {
         feedback.textContent = "La contraseña es poco segura";
         feedback.className = "poco";
-        boton.setAttribute("disabled", "true");
     }
-    return test(password);
+    validar();
 });
 
-boton.addEventListener("click", () => {
+function validar() {
+    // y: && o: ||
+    console.log(passwordInput.value.length);
+    console.log(usuario.value.length);
+    if (passwordInput.value.length >= 11 && usuario.value.length > 0) {
+        // habilitamos el boton
+        boton.removeAttribute("disabled");
+    } else {
+        // deshabilitamos el boton
+        boton.setAttribute("disabled", "true");
+    }
+}
+
+function login(evento) {
+    evento.preventDefault();
     alert("Login...");
-});
+}
+
+boton.addEventListener("click", login);
