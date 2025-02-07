7bdb2837db54750649e07a94a46add1bca32b3de
Inma Palma <inma.palma99@gmail.com>
2025-02-07 12:00:51 +0100

    agregar el boton para acceder con la contraseña superada a 10 numeros

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-const passwordInput = document.getElementById('password');
-const feedback = document.getElementById('feedback');
+const passwordInput = document.getElementById("password");
+const feedback = document.getElementById("feedback");
+const boton = document.getElementById("boton");
 
-passwordInput.addEventListener('input', () => {
-  const password = passwordInput.value;
-  if (password.length <= 7) {
-    feedback.textContent = "La contraseña no es segura";
-    feedback.className = "mal";
-  } else if (password.length >= 11) {
-    feedback.textContent = "La contraseña es segura";
-    feedback.className = "bien";
-  } else {
-    feedback.textContent = "La contraseña es poco segura";
-    feedback.className = "poco";
-  }
-  return test(password);
+passwordInput.addEventListener("input", () => {
+    const password = passwordInput.value;
+    if (password.length <= 7) {
+        feedback.textContent = "La contraseña no es segura";
+        feedback.className = "mal";
+        boton.setAttribute("disabled", "true");
+    } else if (password.length >= 11) {
+        feedback.textContent = "La contraseña es segura";
+        feedback.className = "bien";
+        //habilitar boton
+        boton.removeAttribute("disabled");
+    } else {
+        feedback.textContent = "La contraseña es poco segura";
+        feedback.className = "poco";
+        boton.setAttribute("disabled", "true");
+    }
+    return test(password);
+});
+
+boton.addEventListener("click", () => {
+    alert("Login...");
 });
